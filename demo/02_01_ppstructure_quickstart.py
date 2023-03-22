572ce8bda9b016cbd40de8977f8cc6433ea8aed9
thetime50 <1004763777@qq.com>
2023-03-22 18:24:32 +0800

    structFun

--- demo/02_01_ppstructure_quickstart.py
+++ demo/02_01_ppstructure_quickstart.py
@@ -24,7 +24,8 @@ def dirLayoutTableFun():
     from PIL import Image
 
     # font_path = 'doc/fonts/simfang.ttf' # PaddleOCR下提供字体包
-    font_path = '../PaddleOCRv3/doc/fonts/simfang.ttf' # linux    image = Image.open(img_path).convert('RGB')
+    font_path = '../PaddleOCRv3/doc/fonts/simfang.ttf' # linux    
+    image = Image.open(img_path).convert('RGB')
     im_show = draw_structure_result(image, result,font_path=font_path)
     im_show = Image.fromarray(im_show)
     im_show.save( os.path.join(save_folder,imgFileName,'result.jpg'))
@@ -108,8 +109,33 @@ def tableFun():
     im_show = Image.fromarray(im_show)
     im_show.save(os.path.join(save_folder,imgFileName,'result.jpg'))
 
+def structFun():
+    table_engine = PPStructure(table=False, show_log=True, image_orientation=True)
+
+    save_folder = os.path.join(resDir,'struct')
+    # img_path = os.path.abspath('../PaddleOCRv3/doc/imgs/00018069.jpg')
+    img_path = os.path.abspath('../PaddleOCRv3/doc/imgs/00015504.jpg') # 跑这个数据分享反了
+    imgFileName = os.path.basename(img_path).split('.')[0]
+
+    img = cv2.imread(img_path)
+    result = table_engine(img)
+    save_structure_res(result, save_folder,imgFileName)
+
+    for line in result:
+        line.pop('img')
+        print(line)
+
+    from PIL import Image
+
+    # font_path = 'doc/fonts/simfang.ttf' # PaddleOCR下提供字体包
+    font_path = '../PaddleOCRv3/doc/fonts/simfang.ttf' # linux    
+    image = Image.open(img_path).convert('RGB')
+    im_show = draw_structure_result(image, result,font_path=font_path)
+    im_show = Image.fromarray(im_show)
+    im_show.save( os.path.join(save_folder,imgFileName,'result.jpg'))
 
 # dirLayoutTable()
 # layoutTableFun()
-layoutFun()
-tableFun()
+# layoutFun()
+# tableFun()
+structFun()
